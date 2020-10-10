875ebeeca1b1b507ecf59a3012e75318c08cccf4
RamKri <testinggrounds221@gmail.com>
2020-10-10 12:07:14 +0530

    Upto part 3

---
+++ polls/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Question
+from django.template import loader
+from django.shortcuts import render,get_object_or_404
+
+# Old Index -> 
+# def index(request):
+# 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+# 	template = loader.get_template('polls/index.html')
+# 	context = {
+# 		'latest_question_list':latest_question_list,
+# 	}    
+# 	return HttpResponse(template.render(context,request))
+
+def index(request):
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	context = {	'latest_question_list'	:	latest_question_list }
+	return render(request,'polls/index.html',context)
+
+# Old detail ->
+# def detail(request,question_id):
+# 	try:
+# 		question = Question.objects.get(pk=question_id)
+# 	except Question.DoesNotExist:
+# 		raise Http404("Question Does not Exist")
+# 	return render(request,'polls/details.html',{'question':question})
+
+def detail(request,question_id):
+	question = get_object_or_404(Question,pk=question_id)
+	return render(request,'polls/details.html',{	'question'	:	question	})
+
+def results(request,question_id):
+	return HttpResponse("You're looking at the results of question %s." % question_id)
+
+def vote(request,question_id):
+	return HttpResponse("You're voting  on question %s."% question_id)
