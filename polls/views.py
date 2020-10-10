1089d9d0c5b4149771d5c5ed6dce9d9319eb14b3
RamKri <testinggrounds221@gmail.com>
2020-10-10 17:07:20 +0530

    Before using Generic views

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse,HttpResponseRedirect
+from .models import Question,Choice
 from django.template import loader
 from django.shortcuts import render,get_object_or_404
-
+from django.urls import reverse
 # Old Index -> 
 # def index(request):
 # 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -31,7 +31,24 @@ def detail(request,question_id):
 	return render(request,'polls/details.html',{	'question'	:	question	})
 
 def results(request,question_id):
-	return HttpResponse("You're looking at the results of question %s." % question_id)
+	question = get_object_or_404(Question,pk=question_id)
+	return render(request,'polls/result.html',{'question':question})
 
 def vote(request,question_id):
-	return HttpResponse("You're voting  on question %s."% question_id)
+	question = get_object_or_404(Question,pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except (KeyError,Choice.DoesNotExist):
+		return render(request,'polls:detail.html',{
+			'question':question,
+			'error_message':'Choose an Option'
+			})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		return HttpResponseRedirect(reverse('polls:results',args=(question_id,)))
+#  reverse function helps avoid having to hardcode a URL in the view function.
+#  It is given the name of the view that we want to pass control to and 
+#  the variable portion of the URL pattern that points to that view.
+#  reverse() call will return a string like
+# '/polls/3/results/'
