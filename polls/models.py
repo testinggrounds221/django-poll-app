a049b1f41fd872c5f4a092afe13c567daebe90d6
RamKri <testinggrounds221@gmail.com>
2020-10-11 11:50:19 +0530

    Final Commit on tihs branch

--- polls/models.py
+++ polls/models.py
@@ -7,7 +7,7 @@ class Question(models.Model):
 	pub_date = models.DateField('date published')
 
 	def __str__(self):
-		return '{} on {}'.format(self.question_text,self.pub_date)
+		return self.question_text
 	
 	def was_published_recently(self):
 		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
@@ -18,4 +18,4 @@ class Choice(models.Model):
 	votes = models.IntegerField(default=0)
 
 	def __str__(self):
-		return '{} on {}'.format(self.choice_txt,self.votes)
+		return self.choice_txt
